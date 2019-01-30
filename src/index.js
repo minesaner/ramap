f2bd48673665a1de9ce47fc6166dea615bf5763e
jiajiangxu <minesaner@163.com>
2019-01-30 17:43:44 +0800

    PathSimplifier and PointSimplifier

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import InfoWindow from './InfoWindow'
 import Sider from './Sider'
 import Context from './ContextMap'
 import UI from './ui/UI'
-// import PathSimplifier from './ui/PathSimplifier'
-// import PointSimplifier from './ui/PointSimplifier'
+import PathSimplifier from './ui/PathSimplifier'
+import PointSimplifier from './ui/PointSimplifier'
 import SimpleMarker from './ui/SimpleMarker'
 import Plugin from './plugin/Plugin'
 import ToolBar from './plugin/ToolBar'
@@ -23,8 +23,8 @@ export {
   InfoWindow,
   Context,
   UI,
-  // PathSimplifier,
-  // PointSimplifier,
+  PathSimplifier,
+  PointSimplifier,
   SimpleMarker,
   Plugin,
   ToolBar,
